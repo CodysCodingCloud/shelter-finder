ef6a8ac2f22253b7e0d54ffb05c2c7af77dbaeec
cody <adwingsx@aim.com>
2023-02-28 16:39:22 -0500

    hold

---
+++ frontend/src/components/ShelterView.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../store/hooks';
+import { getShelter } from '../store/reducers/shelterSlice';
+// import { useNavigate } from 'react-router-dom';
+// import { IShelter } from '../../../serversrc/db/Shelter';
+import FormItem from './FormItem';
+
+export default function ShelterForm() {
+  const shelter = useAppSelector((state) => state.shelter.currentShelter);
+  const dispatch = useAppDispatch();
+  // const navigate = useNavigate();
+  // type shelterParams = { id: string };
+  const params: any = useParams();
+  const [formError, setFormError] = useState({});
+  const [disableForm, setDisableForm] = useState(true);
+  React.useEffect(() => {
+    dispatch(getShelter(params.id));
+  }, []);
+
+  return (
+    <div className="single-shelter-container">
+      {shelter._id ? (
+        <div>
+          <img></img>
+          <div>Name</div>
+        </div>
+      ) : (
+        <div>This resource does not exist</div>
+      )}
+    </div>
+  );
+}
