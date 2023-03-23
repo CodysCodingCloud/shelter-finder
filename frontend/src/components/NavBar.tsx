127b204755e4f4762a4d20234f0a9a228daaa58f
cody <adwingsx@aim.com>
2023-03-22 23:39:22 -0400

    navbar update

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -57,48 +57,6 @@ export default function NavBar() {
           </Link>
         </ul>
       </div>
-
-      {/* <div className="navBar">
-        <div className="navLeft">
-          <Link to="/" className="temp-link">
-            Home
-          </Link>
-        </div>
-
-        <div className="navRight">
-          <div>
-            <Link to="/login" className="temp">
-              Login
-            </Link>
-          </div>
-          <div>
-            <Link to="/register" className="temp">
-              Register
-            </Link>
-          </div>{' '}
-          <div>
-            <Link to="/shelter" className="temp">
-              shelter
-            </Link>
-          </div>{' '}
-          <div>
-            <Link to="/newshelter" className="temp">
-              newshelter
-            </Link>
-          </div>{' '}
-          <div>
-            <Link to="/singleview" className="temp">
-              singleview
-            </Link>
-          </div>{' '}
-          <div>
-            <Link to="/shelters" className="temp">
-              shelters
-            </Link>
-          </div>
-          <LogoutButton />
-        </div>
-      </div> */}
     </nav>
   );
 }
